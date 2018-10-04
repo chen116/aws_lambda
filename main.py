69bb84db30af8e05d4dea0d0c4422bbcc774a895
root <chen116@usc.edu>
2018-10-04 17:25:41 -0400

    a

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
 import cv2
+import io
+import numpy
+import base64
 def meow(event, context):
     print("Time remaining (MScvy):", context.get_remaining_time_in_millis())
     return {
