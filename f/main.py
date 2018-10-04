f2550ef9b1d4851578325d7270f3360b633d6396
root <chen116@usc.edu>
2018-10-04 14:00:00 -0400

    a

--- f/main.py
+++ f/main.py
@@ -1,8 +1,9 @@
 import json
 
 def lambda_handler(event, context):
+    print("Time remaining (MS):", context.get_remaining_time_in_millis())
     return {
         "statusCode": 200,
-        "body": json.dumps('Hello from Lambdassss!')
+        "body": json.dumps('dHello from Lambdassss!')
     }
 
