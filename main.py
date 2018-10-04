ff04470319cf10b698aa7400015636476f658673
root <chen116@usc.edu>
2018-10-04 17:47:52 -0400

    a

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ import base64
 def meow(event, context):
 
 
-    nparr = np.fromstring(base64.b64decode(event.body), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    h, w = img.shape[:2]
-    # #To shrink an image, it will generally look best with cv::INTER_AREA interpolation, whereas to enlarge an image, it will generally look best with cv::INTER_CUBIC (slow) or cv::INTER_LINEAR (faster but still looks OK).
-    img = cv2.resize(img, (3*w, 3*h), interpolation = cv2.INTER_CUBIC)   
-    kernel_sharpening = np.array([[-1,-1,-1], [-1, 9,-1],[-1,-1,-1]])
-    img = cv2.filter2D(img, -1, kernel_sharpening)
-    retval, buffer = cv2.imencode('.jpg', img)
-    word = (base64.b64encode(buffer))
-    body = (base64.b64decode(word))
+    # nparr = np.fromstring(base64.b64decode(event.body), np.uint8)
+    # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    # h, w = img.shape[:2]
+    # # #To shrink an image, it will generally look best with cv::INTER_AREA interpolation, whereas to enlarge an image, it will generally look best with cv::INTER_CUBIC (slow) or cv::INTER_LINEAR (faster but still looks OK).
+    # img = cv2.resize(img, (3*w, 3*h), interpolation = cv2.INTER_CUBIC)   
+    # kernel_sharpening = np.array([[-1,-1,-1], [-1, 9,-1],[-1,-1,-1]])
+    # img = cv2.filter2D(img, -1, kernel_sharpening)
+    # retval, buffer = cv2.imencode('.jpg', img)
+    # word = (base64.b64encode(buffer))
+    # body = (base64.b64decode(word))
 
 
     print("Time remaining (MScvy):", context.get_remaining_time_in_millis())
     return {
         "statusCode": 200,
-        "body": json.dumps('dHello from Lambdasssscv2 nowayyyy!')
+        "body": json.dumps(event)
     }
 
 
